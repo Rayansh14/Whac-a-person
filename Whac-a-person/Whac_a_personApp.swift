3c8e75d97a17855a718b93a29c4e56e8756fe2bf
Rayansh14 <rayanshgupta1406@gmail.com>
2021-03-25 10:57:27 +0530

    Initial Commit

---
+++ Whac-a-person/Whac_a_personApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whac_a_personApp.swift
+//  Whac-a-person
+//
+//  Created by Rayansh Gupta on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Whac_a_personApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
